df0832711b39f79c572ac6c698b499c49252f14d
marlisajp <marlisajp@gmail.com>
2023-02-09 16:42:17 -0500

    phase 1 stuff

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
+cd desktop/Full Stack Academy/VsCode/Foundations/Card-Games
+
 # Card-Games
+
+# Phase 1 Base Classes
+
+## Card
+
+Write a Card class with two properties: rank and suit.
+
+## Hand
+
+A Hand has one property: an array of cards.
+
+addCard(card) will add the given card to the hand.
+
+playCard(card) will remove and return the specified card from the hand. It should return null if the card is not in the hand.
+
+## Deck
+
+A Deck has an array of possible card ranks, an array of possible card suits, and an array of cards.
+
+generateCards() will populate the deck with an initial array of cards.
+
+shuffleCards() will shuffle the cards in the deck. Search online for a shuffle function. You do not need to come up with this yourself.
+
+draw() will remove and return a card from the deck.
+
+deal(numHands, cardsPerHand) will deal cardsPerHand cards to numHands hands.
+
+# Phase 2 Game Mechanics
+
+## You will be implementing the logic for a card game of your choice. Some examples of games are:
+
+Uno
+Poker
+Bridge
+Mahjong
+Trading Card Games
+
+### The additional features that you write will depend on which game you chose. Some examples of possible features are:
+
+different ranks and suits
+more Card properties
+more actions in Hand or Deck
+Once you have decided on a game:
+
+## Write a Game class that manages the Deck, Hands, and Cards.
+
+Update Deck, Hand, and Card classes as needed.
+Work on small parts at a time and check in with an instructor frequently. This is a big project and it can be easy to get overwhelmed.
+
+Here are some prompts for getting started:
+
+Keep track of the Deck in Game.
+Does your game keep track of discards?
+Can Hands interact with each other?
+Deal out hands and keep track of them in Game.
+What happens on a player's turn?
+When does a player win or lose?
+
+\*Your game does not have to fully work. This activity is mainly intended for you to think about how classes interact with each other. Move on to Phase 3 even if your Game logic is not perfect so that you will also have a chance to practice using classes to manipulate the DOM.
+
+# Phase 3 DOM
+
+### In this phase, you will be working on visually representing the Game.
+
+If there are elements that are always on the screen, then they should be coded into your HTML. This might include buttons, containers, instructions, etc.
+
+There will also be elements that are dynamic, such as the cards in each player's hand or in the deck. One way to implement dynamic elements is a render function in your Game class.
+
+An example of what render might do is:
+
+Select the container element.
+Use loops and document.createElement to create a new element for each Card.
+Replace the container's children with the generated elements.
+Here are some milestones to hit:
+
+Every card in the Deck is visible on the screen.
+Every card in each Hand is visible on the screen.
+A "Deal" button will deal the cards when it is pressed.
+A "Draw" button adds a card to the next player's hand.
+A "Reset" button resets the Game to an initial state.
+What other mechanics does your game have?
+
+
---
+++ index.js
@@ -0,0 +1,126 @@
+//* Card Class *//
+// Write a Card class with two properties: rank and suit.
+class Card {
+  constructor(rank, suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+}
+
+//* Hand Class *//
+// A Hand has one property: an array of cards.
+class Hand {
+  constructor(cardsArr) {
+    this.cardsArr = cardsArr;
+  }
+
+  // addCard(card) will add the given card to the hand.
+  addCard(card) {
+    this.cardsArr.push(card);
+  }
+
+  // playCard(card) will remove and return the specified card from the hand. It should return null if the card is not in the hand. // -1 is returned if the item is not found in the array
+  playCard(card) {
+    const index = this.cardsArr.indexOf(card);
+    if (index !== -1) {
+      this.cardsArr.splice(index, 1);
+      return card;
+    } else {
+      return null;
+    }
+  }
+}
+
+//* Deck Class *//
+//A Deck has an array of possible card ranks, an array of possible card suits, and an array of cards. 3 array parameters
+class Deck {
+  constructor(ranks, suits, allCards) {
+    this.ranks = ranks;
+    this.suits = suits;
+    this.allCards = allCards;
+  }
+
+  generateCards() {
+    // generateCards() will populate the deck with an initial array of cards.
+    // deck will be 13 ranks with 4 suits each (52 cards)
+    this.ranks = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
+    this.suits = ["spades", "hearts", "diamonds", "clubs"];
+    // for (const suit of this.suits) {
+    //   // goes through all suits
+    //   for (const rank of this.ranks) {
+    //     // goes through all ranks
+    //     this.allCards.push(new Card(rank, suit)); //adds new card instance to array of allcards
+    //   }
+    // }
+
+    for (let i = 0; i < this.suits.length; i++) { // i = index of suits array
+      for (let j = 0; j < this.ranks.length; j++) { // j = index of ranks array
+        this.allCards.push(new Card(this.ranks[j], this.suits[i]));
+      }
+    }
+  }
+
+  shuffleCards() {
+    // fisher yates shuffle// shuffleCards() will shuffle the cards in the deck. Search online for a shuffle function. You do not need to come up with this yourself.
+    for (let i = this.allCards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.allCards[i], this.allCards[j]] = [ // swapping two cards(two indexes) from the array every time the loop repeats. shuffle 2 cards 52 times
+        this.allCards[j],
+        this.allCards[i],
+      ];
+    }
+  }
+
+  draw() {
+    // draw() will remove and return a card from the deck.
+    this.allCards.shift();
+  }
+}
+
+// deal(numHands, cardsPerHand) will deal cardsPerHand cards to numHands hands.
+
+//***************** TESTING AREA ***********************//
+
+// creates new card
+const firstCard = new Card("A", "spades");
+const secondCard = new Card("3", "hearts");
+const thirdCard = new Card("J", "clubs");
+const fourthCard = new Card("5", "diamonds");
+
+// adds new card to hand
+let hand = new Hand([]);
+
+hand.addCard(firstCard); //! WORKS
+hand.addCard(secondCard);
+hand.addCard(thirdCard);
+hand.addCard(fourthCard);
+console.log(hand);
+
+hand.playCard(thirdCard); //! WORKS
+console.log(hand);
+//Hand cards: [Card { rank: 'A', suit: 'spades' },Card { rank: '3', suit: 'hearts' }]
+
+const deck = new Deck([], [], []); //new instance of deck with 3 arrays
+
+deck.generateCards(); //! WORKS
+console.log(deck.allCards);
+
+// deck.shuffleCards(); // shuffles deck //! WORKS
+// console.log(deck.allCards);
+
+// deck.draw(); // removes a card from the deck  //! WORKS
+// console.log(deck.allCards);
